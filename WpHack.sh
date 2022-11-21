98f3ce94565cb74c2cdaaddc72bfeaa5bfcb4776
Lokesh Kumar <109752684+trmxvibs@users.noreply.github.com>
2022-11-21 11:09:24 +0530

    Add files via upload

---
+++ WpHack.sh
@@ -0,0 +1,400 @@
+#                             Online Bash Shell.
+# Updating package list of termux
+# tool made for fun purpose only.
+
+apt-get update -y
+apt-get upgrade -y
+clear
+printf "                \e[101m\e[1;77m package upgrading successfully \e[0m\n" 
+sleep 3.0
+##  checking Dependices for Installing
+printf "\n"
+printf "                  \e[100m\e[1;70m checking Dependices for System \e[0m\n"
+apt-get install python python2 python3 git php openssl openssl-tool curl wget -y
+sleep 3.0
+clear
+echo -e '\e[91m
+██╗    ██╗██╗  ██╗ █████╗ ████████╗███████╗ █████╗ ██████╗ ██████╗ \e[91m
+██║    ██║██║  ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗\e[91m
+██║ █╗ ██║███████║███████║   ██║   ███████╗███████║██████╔╝██████╔╝\e[91m
+██║███╗██║██╔══██║██╔══██║   ██║   ╚════██║██╔══██║██╔═══╝ ██╔═══╝ \e[91m
+╚███╔███╔╝██║  ██║██║  ██║   ██║   ███████║██║  ██║██║     ██║     \e[91m
+                       V.1.01    \e[91m
+                                                                   
+██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗  \e[91m             
+██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝  \e[91m             
+███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗ \e[91m            
+██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║ \e[91m            
+██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝ \e[91m            
+  Join my youtube channel https://youtube.com/@termux2
+\e[0m'
+printf "                \e[101m\e[1;77m  >> Script by MR. Lokesh << \e[0m\n"
+printf "\n"
+printf "       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m termux2 \e[100m\e[1;77m << \e[0m\n"
+printf "\n"
+sleep 3.0
+echo         " Cheking System Supporting files"
+sleep 2.0
+clear
+echo "Checking."
+sleep 0.5
+clear
+echo "cHecking.."
+sleep 0.5
+clear
+echo "chEcking..."
+sleep 0.5
+clear
+echo "cheCking...."
+sleep 0.5
+clear
+echo "checKing....."
+sleep 0.5
+clear
+echo "checkIng......"
+sleep 0.5
+clear
+echo "checkiNg......."
+sleep 0.5
+clear
+echo "checkinG........"
+clear
+sleep 0.5
+echo "Checking."
+sleep 0.5
+clear
+echo "cHecking.."
+sleep 0.5
+clear
+echo "chEcking..."
+sleep 0.5
+clear
+echo "cheCking...."
+sleep 0.5
+clear
+echo "checKing....."
+sleep 0.5
+clear
+echo "checkIng......"
+sleep 0.5
+clear
+echo "checkiNg......."
+sleep 0.5
+clear
+echo "checkinG........"
+clear
+sleep 0.5
+echo "Checking."
+sleep 0.5
+clear
+echo "cHecking.."
+sleep 0.5
+clear
+echo "chEcking..."
+sleep 0.5
+clear
+echo "cheCking...."
+sleep 0.5
+clear
+echo "checKing....."
+sleep 0.5
+clear
+echo "checkIng......"
+sleep 0.5
+clear
+echo "checkiNg......."
+sleep 0.5
+clear
+echo "checkinG........"
+clear
+sleep 0.5
+echo "Checking."
+sleep 0.5
+clear
+echo "cHecking.."
+sleep 0.5
+clear
+echo "chEcking..."
+sleep 0.5
+clear
+echo "cheCking...."
+sleep 0.5
+clear
+echo "checKing....."
+sleep 0.5
+clear
+echo "checkIng......"
+sleep 0.5
+clear
+echo "checkiNg......."
+sleep 0.5
+clear
+echo "checkinG........"
+clear
+sleep 0.5
+echo "Checking."
+sleep 0.5
+clear
+echo "cHecking.."
+sleep 0.5
+clear
+echo "chEcking..."
+sleep 0.5
+clear
+echo "cheCking...."
+sleep 0.5
+clear
+echo "checKing....."
+sleep 0.5
+clear
+echo "checkIng......"
+sleep 0.5
+clear
+echo "checkiNg......."
+sleep 0.5
+clear
+echo "checkinG........"
+clear
+sleep 0.5
+echo "Checking."
+sleep 0.5
+clear
+echo "cHecking.."
+sleep 0.5
+clear
+echo "chEcking..."
+sleep 0.5
+clear
+echo "cheCking...."
+sleep 0.5
+clear
+echo "checKing....."
+sleep 0.5
+clear
+echo "checkIng......"
+sleep 0.5
+clear
+echo "checkiNg......."
+sleep 0.5
+clear
+echo "checkinG........"
+clear
+sleep 0.5
+echo "Checking."
+sleep 0.5
+clear
+echo "cHecking.."
+sleep 0.5
+clear
+echo "chEcking..."
+sleep 0.5
+clear
+echo "cheCking...."
+sleep 0.5
+clear
+echo "checKing....."
+sleep 0.5
+clear
+echo "checkIng......"
+sleep 0.5
+clear
+echo "checkiNg......."
+sleep 0.5
+clear
+echo "checkinG........"
+clear
+sleep 0.5
+echo "Checking."
+sleep 0.5
+clear
+echo "cHecking.."
+sleep 0.5
+clear
+echo "chEcking..."
+sleep 0.5
+clear
+echo "cheCking...."
+sleep 0.5
+clear
+echo "checKing....."
+sleep 0.5
+clear
+echo "checkIng......"
+sleep 0.5
+clear
+echo "checkiNg......."
+sleep 0.5
+clear
+echo "checkinG........"
+clear
+sleep 0.5
+echo "Checking."
+sleep 0.5
+clear
+echo "cHecking.."
+sleep 0.5
+clear
+echo "chEcking..."
+sleep 0.5
+clear
+echo "cheCking...."
+sleep 0.5
+clear
+echo "checKing....."
+sleep 0.5
+clear
+echo "checkIng......"
+sleep 0.5
+clear
+echo "checkiNg......."
+sleep 0.5
+clear
+echo "checkinG........"
+clear
+sleep 0.5
+echo "Checking."
+sleep 0.5
+clear
+echo "cHecking.."
+sleep 0.5
+clear
+echo "chEcking..."
+sleep 0.5
+clear
+echo "cheCking...."
+sleep 0.5
+clear
+echo "checKing....."
+sleep 0.5
+clear
+echo "checkIng......"
+sleep 0.5
+clear
+echo "checkiNg......."
+sleep 0.5
+clear
+echo "checkinG........"
+clear
+sleep 0.5
+printf "        \e[101m\e[1;77m  >> All is Well << \e[0m\n"
+sleep 5
+printf "\n"
+printf "        \e[101m\e[1;77m  >> Upgrading Supporting tools << \e[0m\n"
+sleep 5
+pip install --upgrade pip
+pip2 install --upgrade pip
+pip install wheel
+pip install setuptools
+pip install RUST
+pip install firebase-admin
+sleep 10
+clear
+printf "\n"
+printf "   \e[100m\e[1;70m +++ Supporting tools Installing complete +++ \e[0m\n"
+printf "\n"
+sleep 5
+clear
+echo -e '\e[91m
+██╗    ██╗██╗  ██╗ █████╗ ████████╗███████╗ █████╗ ██████╗ ██████╗ \e[91m
+██║    ██║██║  ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗\e[91m
+██║ █╗ ██║███████║███████║   ██║   ███████╗███████║██████╔╝██████╔╝\e[91m
+██║███╗██║██╔══██║██╔══██║   ██║   ╚════██║██╔══██║██╔═══╝ ██╔═══╝ \e[91m
+╚███╔███╔╝██║  ██║██║  ██║   ██║   ███████║██║  ██║██║     ██║     \e[91m
+                       V.1.01    \e[91m
+                                                                   
+██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗  \e[91m             
+██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝  \e[91m             
+███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗ \e[91m            
+██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║ \e[91m            
+██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝ \e[91m            
+  Join my youtube channel https://youtube.com/@termux2
+                                                        \e[0m'
+printf "\n"
+printf "\e[101m\e[1;77m  >>Script by MR. Lokesh<< \e[0m\n"
+printf "\n"
+printf "\e[100m\e[1;77m >>Youtube Channel : \e[1;96m termux2 \e[100m\e[1;77m<< \e[0m\n"
+printf "\n"
+menu1(){
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Bypass OTP \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Hack with IP Address \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Hack with link\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m HAck with Web \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m HAck with QR code\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m6\e[0m\e[1;92m]\e[0m\e[1;93m Update tool\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m7\e[0m\e[1;92m]\e[0m\e[1;93m Subscriber my Channel\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m8\e[0m\e[1;92m]\e[0m\e[1;93m Chat with me\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m9\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n" 
+    }
+{
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Bypass OTP \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Hack with IP Address \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Hack with link\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m HAck with Web \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m HAck with QR code\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m6\e[0m\e[1;92m]\e[0m\e[1;93m Update tool\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m7\e[0m\e[1;92m]\e[0m\e[1;93m Subscriber my Channel\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m8\e[0m\e[1;92m]\e[0m\e[1;93m Chat with me\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m9\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
+}
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+if [[ $option == 1 || $option == 01 ]]; then
+echo     "Checking Root........"
+sleep 10
+echo -e "\e[91mYOUR\e[96m Device not Root, tool isn't working.
+\e[92m Root\e[96m your TErmux and try again \e[0m "
+sleep 05
+xdg-open https://youtu.be/ChAQaVaJDvg
+sleep 90
+exit
+elif [[ $option == 2 || $option == 02 ]]; then
+echo     "Checking Root........"
+sleep 10
+echo -e "\e[91mYOUR\e[96m Device not Root, tool isn't working.
+\e[92m Root\e[96m your TErmux and try again \e[0m "
+sleep 05
+xdg-open https://youtu.be/ChAQaVaJDvg
+sleep 90
+exit
+elif [[ $option == 3 || $option == 03 ]]; then
+echo     "Checking Root........"
+sleep 10
+echo -e "\e[91mYOUR\e[96m Device not Root, tool isn't working.
+\e[92m Root\e[96m your TErmux and try again \e[0m "
+sleep 05
+xdg-open https://youtu.be/ChAQaVaJDvg
+sleep 90
+exit
+elif [[ $option == 4 || $option == 04 ]]; then
+echo     "Checking Root........"
+sleep 10
+echo -e "\e[91mYOUR\e[96m Device not Root, tool isn't working.
+\e[92m Root\e[96m your TErmux and try again \e[0m "
+sleep 05
+xdg-open https://youtu.be/ChAQaVaJDvg
+sleep 90
+exit
+elif [[ $option == 5 || $option == 05 ]]; then
+echo     "Checking Root........"
+sleep 10
+echo -e "\e[91mYOUR\e[96m Device not Root, tool isn't working.
+\e[92m Root\e[96m your TErmux and try again \e[0m "
+sleep 05
+xdg-open https://youtu.be/ChAQaVaJDvg
+sleep 90
+exit
+elif [[ $option == 6 || $option == 06 ]]; then
+bash update.sh
+elif [[ $option == 7 || $option == 07 ]]; then
+xdg-open https://youtube.com/@termux2
+elif [[ $option == 8 || $option == 08 ]]; then
+xdg-open https://chat.whatsapp.com/DvP0dQe8kA8KrdpruPXlfw
+elif [[ $option == 9 || $option == 09 ]]; then
+exit
+else
+echo
+printf "\e[1;93m [>!<] Invalid Selection!\e[0m\n"
+sleep 3
+echo
+fi
+exit
+}
