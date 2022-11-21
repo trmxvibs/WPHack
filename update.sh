38ab1a02ef9bbc8b4d427382ba23e56d2c72eccb
Lokesh Kumar <109752684+trmxvibs@users.noreply.github.com>
2022-11-21 11:13:40 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,14 @@
+echo    "Checking updates[++++]"
+sleep 10
+printf "\n"
+echo    "Update is avilable[.]"
+sleep 1
+printf "\n"
+echo    "Updating...[+++++]"
+sleep 30
+printf "\n"
+clear
+echo "Update secessfully newest version..[+++]"
+sleep 2
+exit
+exit
