a7b3a58e3a4fc183c6a922d6601c553e9a393c6e
Lokesh Kumar <109752684+trmxvibs@users.noreply.github.com>
2022-11-22 20:07:08 +0530

    Add files via upload

---
+++ wp.sh
@@ -0,0 +1,124 @@
+echo -e '\e[91m
+██╗    ██╗██╗  ██╗ █████╗ ████████╗███████╗ █████╗ ██████╗ ██████╗ \e[91m
+██║    ██║██║  ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗\e[91m
+██║ █╗ ██║███████║███████║   ██║   ███████╗███████║██████╔╝██████╔╝\e[91m
+██║███╗██║██╔══██║██╔══██║   ██║   ╚════██║██╔══██║██╔═══╝ ██╔═══╝ \e[91m
+╚███╔███╔╝██║  ██║██║  ██║   ██║   ███████║██║  ██║██║     ██║     \e[91m
+                       V.1.01    \e[91m
+                                                                   
+██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗  \e[91m             
+██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝  \e[91m             
+███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗ \e[91m            
+██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║ \e[91m            
+██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝ \e[91m            
+  Join my youtube channel https://youtube.com/@termux2
+                                                        \e[0m'
+printf "\n"
+printf "\e[101m\e[1;77m  >>Script by MR. Lokesh<< \e[0m\n"
+printf "\n"
+printf "\e[100m\e[1;77m >>Youtube Channel : \e[1;96m termux2 \e[100m\e[1;77m<< \e[0m\n"
+printf "\n"
+menu1(){
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Bypass OTP \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Hack with IP Address \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Hack with link\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m HAck with Web \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m HAck with QR code\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m6\e[0m\e[1;92m]\e[0m\e[1;93m Update tool\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m7\e[0m\e[1;92m]\e[0m\e[1;93m Subscriber my Channel\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m8\e[0m\e[1;92m]\e[0m\e[1;93m Chat with me\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m9\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n" 
+    }
+{
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Bypass OTP \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Hack with IP Address \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Hack with link\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m HAck with Web \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m HAck with QR code\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m6\e[0m\e[1;92m]\e[0m\e[1;93m Update tool\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m7\e[0m\e[1;92m]\e[0m\e[1;93m Subscriber my Channel\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m8\e[0m\e[1;92m]\e[0m\e[1;93m Chat with me\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m9\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
+}
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+if [[ $option == 1 || $option == 01 ]]; then
+printf    "\e[0;36m Checking Root.......\e[0m\n"
+sleep 10
+echo "ERROR" | figlet | lolcat
+echo -e "\e[31m ++++ Root Not Found ++++\e[0m\n"]
+printf "\e[1;33m ==>> Root your Termux & try again\e[0m\n"
+sleep 05
+xdg-open https://youtu.be/ChAQaVaJDvg
+sleep 40
+xdg-open https://www.youtube.com/channel/UC2MKFsqqSNX-ZGgb8pUKAKQ/?sub_confirmation=1
+sleep 40
+clear
+bash wp.sh
+elif [[ $option == 2 || $option == 02 ]]; then
+printf    "\e[0;36m Checking Root.......\e[0m\n"
+sleep 10
+echo "ERROR" | figlet | lolcat
+echo -e "\e[31m ++++ Root Not Found ++++\e[0m\n"]
+printf "\e[1;33m ==>> Root your Termux & try again\e[0m\n"
+sleep 05
+xdg-open https://youtu.be/ChAQaVaJDvg
+sleep 40
+xdg-open https://www.youtube.com/channel/UC2MKFsqqSNX-ZGgb8pUKAKQ/?sub_confirmation=1
+sleep 40
+clear
+bash wp.sh
+elif [[ $option == 3 || $option == 03 ]]; then
+printf    "\e[0;36m Checking Root.......\e[0m\n"
+sleep 10
+echo "ERROR" | figlet | lolcat
+echo -e "\e[31m ++++ Root Not Found ++++\e[0m\n"]
+printf "\e[1;33m ==>> Root your Termux & try again\e[0m\n"
+sleep 05
+xdg-open https://youtu.be/ChAQaVaJDvg
+sleep 40
+xdg-open https://www.youtube.com/channel/UC2MKFsqqSNX-ZGgb8pUKAKQ/?sub_confirmation=1
+sleep 40
+clear
+bash wp.sh
+elif [[ $option == 4 || $option == 04 ]]; then
+printf    "\e[0;36m Checking Root.......\e[0m\n"
+sleep 10
+echo "ERROR" | figlet | lolcat
+echo -e "\e[31m ++++ Root Not Found ++++\e[0m\n"]
+printf "\e[1;33m ==>> Root your Termux & try again\e[0m\n"
+sleep 05
+xdg-open https://youtu.be/ChAQaVaJDvg
+sleep 40
+xdg-open https://www.youtube.com/channel/UC2MKFsqqSNX-ZGgb8pUKAKQ/?sub_confirmation=1
+sleep 40
+clear
+bash wp.sh
+elif [[ $option == 5 || $option == 05 ]]; then
+printf    "\e[0;36m Checking Root.......\e[0m\n"
+sleep 10
+echo "ERROR" | figlet | lolcat
+echo -e "\e[31m ++++ Root Not Found ++++\e[0m\n"]
+printf "\e[1;33m ==>> Root your Termux & try again\e[0m\n"
+sleep 05
+xdg-open https://youtu.be/ChAQaVaJDvg
+sleep 40
+xdg-open https://www.youtube.com/channel/UC2MKFsqqSNX-ZGgb8pUKAKQ/?sub_confirmation=1
+sleep 40
+clear
+bash wp.sh
+elif [[ $option == 6 || $option == 06 ]]; then
+bash update.sh
+elif [[ $option == 7 || $option == 07 ]]; then
+xdg-open https://youtube.com/@termux2
+elif [[ $option == 8 || $option == 08 ]]; then
+xdg-open https://chat.whatsapp.com/DvP0dQe8kA8KrdpruPXlfw
+elif [[ $option == 9 || $option == 09 ]]; then
+exit
+else
+echo
+printf "\e[1;93m [>!<] Invalid Selection!\e[0m\n"
+sleep 3
+echo
+fi
+exit
+}
