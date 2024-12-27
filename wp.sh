e5294bda6d1b8a495c2c0005d1564a375993aa81
Lokesh Kumar <109752684+trmxvibs@users.noreply.github.com>
2024-12-27 17:47:39 +0530

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -1,125 +1,102 @@
 clear
-echo -e '\e[91m
-██╗    ██╗██╗  ██╗ █████╗ ████████╗███████╗ █████╗ ██████╗ ██████╗ \e[91m
-██║    ██║██║  ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗\e[91m
-██║ █╗ ██║███████║███████║   ██║   ███████╗███████║██████╔╝██████╔╝\e[91m
-██║███╗██║██╔══██║██╔══██║   ██║   ╚════██║██╔══██║██╔═══╝ ██╔═══╝ \e[91m
-╚███╔███╔╝██║  ██║██║  ██║   ██║   ███████║██║  ██║██║     ██║     \e[91m
-                       V.1.01    \e[91m
-                                                                   
-██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗  \e[91m             
-██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝  \e[91m             
-███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗ \e[91m            
-██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║ \e[91m            
-██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝ \e[91m            
-  Join my youtube channel https://youtube.com/@termux2
-                                                        \e[0m'
-printf "\n"
-printf "\e[101m\e[1;77m  >>Script by MR. Lokesh<< \e[0m\n"
-printf "\n"
-printf "\e[100m\e[1;77m >>Youtube Channel : \e[1;96m termux2 \e[100m\e[1;77m<< \e[0m\n"
-printf "\n"
+echo -e '\e[92m
+██████╗ ███████╗██╗  ██╗ █████╗ ████████╗███████╗ █████╗ ██████╗ ██████╗ \e[94m
+██╔══██╗██╔══██╗██║  ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗\e[94m
+██████╔╝███████║███████║███████║   ██║   ███████╗███████║██████╔╝██████╔╝\e[94m
+██╔═══╝ ██╔══██║██╔══██║██╔══██║   ██║   ╚════██║██╔══██║██╔═══╝ ██╔═══╝ \e[94m
+██║     ██║  ██║██║  ██║██║  ██║   ██║   ███████║██║  ██║██║     ██║     \e[94m
+                          V.1.01    \e[92m
+                                 
+██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗  \e[92m              
+██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝  \e[92m              
+███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗ \e[92m            
+██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║ \e[92m            
+██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝ \e[92m            
+      WARNING: This is a fake repo. It does not work as expected. \e[91m
+    Enjoy the show, but proceed with caution! \e[91m
+  \e[92mJoin my youtube channel: https://youtube.com/@termux2\e[0m
+'
+
+# Adding a professional-looking footer
+footer() {
+    echo -e "\n\e[1;93m█████████████████████████████████████████████████████████████████████\e[0m"
+    echo -e "\e[1;96m    Script by: MR. Lokesh Kumar - Always improving with your support! \e[0m"
+    echo -e "\e[1;96m    Subscribe to the channel: https://youtube.com/@termux2 \e[0m"
+    echo -e "\e[1;93m█████████████████████████████████████████████████████████████████████\e[0m"
+}
+
+footer # Display footer
+
+# Main Menu
 menu1(){
-printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Bypass OTP \e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Hack with IP Address \e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Hack with link\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m HAck with Web \e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m HAck with QR code\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m6\e[0m\e[1;92m]\e[0m\e[1;93m Update tool\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m7\e[0m\e[1;92m]\e[0m\e[1;93m Subscriber my Channel\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m8\e[0m\e[1;92m]\e[0m\e[1;93m Chat with me\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m9\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n" 
-    }
-{
-printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Bypass OTP \e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Hack with IP Address \e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Hack with link\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m HAck with Web \e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m HAck with QR code\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m6\e[0m\e[1;92m]\e[0m\e[1;93m Update tool\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m7\e[0m\e[1;92m]\e[0m\e[1;93m Subscriber my Channel\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m8\e[0m\e[1;92m]\e[0m\e[1;93m Chat with me\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m9\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
+    echo -e "\n\e[1;92m[01] \e[1;33mBypass OTP \e[0m"
+    echo -e "\e[1;92m[02] \e[1;33mHack with IP Address \e[0m"
+    echo -e "\e[1;92m[03] \e[1;33mHack with Link \e[0m"
+    echo -e "\e[1;92m[04] \e[1;33mHack with Web \e[0m"
+    echo -e "\e[1;92m[05] \e[1;33mHack with QR Code \e[0m"
+    echo -e "\e[1;92m[06] \e[1;33mUpdate Tool \e[0m"
+    echo -e "\e[1;92m[07] \e[1;33mSubscribe to my Channel \e[0m"
+    echo -e "\e[1;92m[08] \e[1;33mChat with Me \e[0m"
+    echo -e "\e[1;92m[09] \e[1;33mExit \e[0m"
 }
+
+menu1 # Display menu
+
+# Function for user input
 read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+
+# Processing User Input
 if [[ $option == 1 || $option == 01 ]]; then
-printf    "\e[0;36m Checking Root.......\e[0m\n"
-sleep 10
-echo "ERROR" | figlet | lolcat
-echo -e "\e[31m ++++ Root Not Found ++++\e[0m\n"]
-printf "\e[1;33m ==>> Root your Termux & try again\e[0m\n"
-sleep 05
-xdg-open https://youtu.be/ChAQaVaJDvg
-sleep 40
-xdg-open https://www.youtube.com/channel/UC2MKFsqqSNX-ZGgb8pUKAKQ/?sub_confirmation=1
-sleep 40
-clear
-bash wp.sh
+    echo "Bypassing OTP... [This is a fake feature!]"
+    sleep 2
+    bash wp.sh
 elif [[ $option == 2 || $option == 02 ]]; then
-printf    "\e[0;36m Checking Root.......\e[0m\n"
-sleep 10
-echo "ERROR" | figlet | lolcat
-echo -e "\e[31m ++++ Root Not Found ++++\e[0m\n"]
-printf "\e[1;33m ==>> Root your Termux & try again\e[0m\n"
-sleep 05
-xdg-open https://youtu.be/ChAQaVaJDvg
-sleep 40
-xdg-open https://www.youtube.com/channel/UC2MKFsqqSNX-ZGgb8pUKAKQ/?sub_confirmation=1
-sleep 40
-clear
-bash wp.sh
+    echo "Hacking with IP... [This is a fake feature!]"
+    sleep 2
+    bash wp.sh
 elif [[ $option == 3 || $option == 03 ]]; then
-printf    "\e[0;36m Checking Root.......\e[0m\n"
-sleep 10
-echo "ERROR" | figlet | lolcat
-echo -e "\e[31m ++++ Root Not Found ++++\e[0m\n"]
-printf "\e[1;33m ==>> Root your Termux & try again\e[0m\n"
-sleep 05
-xdg-open https://youtu.be/ChAQaVaJDvg
-sleep 40
-xdg-open https://www.youtube.com/channel/UC2MKFsqqSNX-ZGgb8pUKAKQ/?sub_confirmation=1
-sleep 40
-clear
-bash wp.sh
+    echo "Hacking with Link... [This is a fake feature!]"
+    sleep 2
+    bash wp.sh
 elif [[ $option == 4 || $option == 04 ]]; then
-printf    "\e[0;36m Checking Root.......\e[0m\n"
-sleep 10
-echo "ERROR" | figlet | lolcat
-echo -e "\e[31m ++++ Root Not Found ++++\e[0m\n"]
-printf "\e[1;33m ==>> Root your Termux & try again\e[0m\n"
-sleep 05
-xdg-open https://youtu.be/ChAQaVaJDvg
-sleep 40
-xdg-open https://www.youtube.com/channel/UC2MKFsqqSNX-ZGgb8pUKAKQ/?sub_confirmation=1
-sleep 40
-clear
-bash wp.sh
+    echo "Hacking with Web... [This is a fake feature!]"
+    sleep 2
+    bash wp.sh
 elif [[ $option == 5 || $option == 05 ]]; then
-printf    "\e[0;36m Checking Root.......\e[0m\n"
-sleep 10
-echo "ERROR" | figlet | lolcat
-echo -e "\e[31m ++++ Root Not Found ++++\e[0m\n"]
-printf "\e[1;33m ==>> Root your Termux & try again\e[0m\n"
-sleep 05
-xdg-open https://youtu.be/ChAQaVaJDvg
-sleep 40
-xdg-open https://www.youtube.com/channel/UC2MKFsqqSNX-ZGgb8pUKAKQ/?sub_confirmation=1
-sleep 40
-clear
-bash wp.sh
+    echo "Hacking with QR Code... [This is a fake feature!]"
+    sleep 2
+    bash wp.sh
 elif [[ $option == 6 || $option == 06 ]]; then
-bash update.sh
+    bash update.sh
 elif [[ $option == 7 || $option == 07 ]]; then
-xdg-open https://youtube.com/@termux2
+    xdg-open https://youtube.com/@termux2
 elif [[ $option == 8 || $option == 08 ]]; then
-xdg-open https://chat.whatsapp.com/DvP0dQe8kA8KrdpruPXlfw
+    xdg-open https://chat.whatsapp.com/DvP0dQe8kA8KrdpruPXlfw
 elif [[ $option == 9 || $option == 09 ]]; then
-exit
+    exit
 else
-echo
-printf "\e[1;93m [>!<] Invalid Selection!\e[0m\n"
-sleep 3
-echo
+    echo -e "\n\e[1;91m[ERROR] Invalid selection. Please try again.\e[0m"
+    sleep 2
+    bash script.sh # Rerun the script
 fi
-exit
+
+# Additional amazing features
+quote_of_the_day() {
+    echo -e "\n\e[1;93m\"The only way to do great work is to love what you do.\"\e[0m"
+    echo -e "\e[1;96m - Steve Jobs\e[0m"
 }
+
+quote_of_the_day # Display motivational quote
+
+# Final professional footer with a dynamic date
+footer() {
+    current_date=$(date '+%Y-%m-%d %H:%M:%S')
+    echo -e "\n\e[1;93m█████████████████████████████████████████████████████████████████████\e[0m"
+    echo -e "\e[1;96m    Script by: MR. Lokesh Kumar | Date: $current_date \e[0m"
+    echo -e "\e[1;96m    Subscribe to the channel: https://youtube.com/@termux2 \e[0m"
+    echo -e "\e[1;93m█████████████████████████████████████████████████████████████████████\e[0m"
+}
+
+footer # Display the footer again
+
+exit
